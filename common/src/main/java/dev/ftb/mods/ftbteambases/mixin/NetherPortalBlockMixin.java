822942d052a4daa32fede1b438f1b446e6e04745
Des Herriott <des.herriott@gmail.com>
2025-02-03 15:52:27 +0000

    fix: ensure custom nether portal Y pos applies always
    Extra mixin to work when team-specific nether portals are off

--- common/src/main/java/dev/ftb/mods/ftbteambases/mixin/NetherPortalBlockMixin.java
+++ common/src/main/java/dev/ftb/mods/ftbteambases/mixin/NetherPortalBlockMixin.java
@@ -7,12 +7,17 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.NetherPortalBlock;
 import net.minecraft.world.level.portal.DimensionTransition;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NetherPortalBlock.class)
+@Debug(export = true)
 public abstract class NetherPortalBlockMixin {
     @Inject(method="getPortalDestination", at = @At("HEAD"), cancellable = true)
     public void onGetPortalDestination(ServerLevel serverLevel, Entity entity, BlockPos blockPos, CallbackInfoReturnable<DimensionTransition> cir) {
@@ -22,5 +27,12 @@ public abstract class NetherPortalBlockMixin {
                 cir.setReturnValue(transition);
             }
         }
+    }
+
+    @ModifyVariable(method = "getExitPortal", at = @At("HEAD"), argsOnly = true, index = 4)
+    private BlockPos modifyExitPos(BlockPos exitPos) {
+        return !ServerConfig.TEAM_SPECIFIC_NETHER_ENTRY_POINT.get() && ServerConfig.USE_CUSTOM_PORTAL_Y_POS.get() ?
+                new BlockPos(exitPos.getX(), ServerConfig.CUSTOM_PORTAL_Y_POS.get(), exitPos.getZ()) :
+                exitPos;
     }
 }
